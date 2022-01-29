e6ce701c888501d17d7c7883225cdb6858c648d8
Henri Piron <hepi@odoo.com>
2022-01-29 12:18:35 +0100

    [ADD] Fibo for temp testing

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+
+def fibo(n):
+    i = 0
+    current = 0
+    previous = 1
+    for i in range(1, n):
+        temp = current
+        current += previous
+        previous = temp
+        print(current)
+
+
+if __name__=="__main__":
+    n = int(sys.argv[1])
+    # print(arg)
+    fibo(n)
+
